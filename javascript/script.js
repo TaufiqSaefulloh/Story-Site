2718303dcd90102dc30effab4ef73c2266b45406
Taufiqsaefulloh20102237 <20102237@ittelkom-pwt.ac.id>
2023-03-07 12:05:24 +0700

    Story Site

---
+++ javascript/script.js
@@ -0,0 +1,30 @@
+// navbar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+// alert
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('Thank you for visiting! If you have any questions, contact us using "SEND ME MESSAGE".', 'success')
+  })
+}
